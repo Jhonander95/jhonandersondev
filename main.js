45c5fb2f1493c8ef2c99f7823c60f8803929a6a0
Jhonanderson Tellez <tellezjhonander@gmail.com>
2023-02-09 22:56:32 -0500

    Botton section portafolio

--- main.js
+++ main.js
@@ -2,20 +2,24 @@ $(document).ready(function(){
   $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
+    autoplay: true,
+    autoplayTimeout: 3000,
     responsiveClass:true,
     responsive:{
       0:{
         items:1,
-        nav:true
+        nav:true,
+        loop:true
       },
       600:{
         items:1,
-        nav:false
+        nav:false,
+        loop:true
       },
       1000:{
         items:1,
         nav:true,
-        loop:false
+        loop:true
       }
     }
   });
